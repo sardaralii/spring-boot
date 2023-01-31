b57146e01a273504b18ae092df11710a50e92728
joykellandra <mbakopsteph@gmail.com>
2023-01-31 12:59:15 -0500

    first commit

---
+++ src/main/java/com/test/springboot/Application.java
@@ -0,0 +1,21 @@
+package com.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hello World Spring Boot Test App!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
